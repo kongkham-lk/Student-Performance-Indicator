fb71db00015fa5acd0124b9b18e0ac13df5e3efd
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2025-09-04 17:52:38 -0700

    let the home page as predict page right away

--- app.py
+++ app.py
@@ -5,11 +5,7 @@ from src.exception import CustomException
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/predict', methods=['GET','POST'])
+@app.route('/', methods=['GET','POST'])
 def predict():
     if request.method == 'POST':
         try:
@@ -35,4 +31,4 @@ def predict():
         return render_template('home.html')
     
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port='8080')
